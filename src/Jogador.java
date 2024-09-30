9209a293f6c9c139fff31df563ce3d1e36ad38a6
Guilherme Oliszewski <guilhermeoliszewski@outlook.com>
2024-09-30 09:32:25 -0300

    implementação da classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,4 +1,51 @@
-public class Jogador {
+import java.util.ArrayList;
 
+public class Jogador {
+    private String email;
+    private String nomeCompleto;
+    private int pin;
+    private ArrayList<Item> itens;
+
+    public Jogador(String email, String nomeCompleto, int pin){
+        this.email = email;
+        this.nomeCompleto = nomeCompleto;
+        this.pin = pin;
+        this.itens = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+    public void addItem(Item item){
+        itens.add(item);
+    }
+    public void removerItem(Item item){
+        itens.remove(item);
+    }
+    public void listarItensOrdenados(){
+        for(int i = 0;i<itens.size();i++){
+            for(int j = 0;j< itens.size();j++){
+                if(itens.get(j).getNome().compareTo(itens.get(j+1).getNome())>0){
+                    Item aux = itens.get(j);
+                    itens.set(j,itens.get(j+1));
+                    itens.set(j+1,aux);
+                }
+            }
+        }
+        System.out.println("Itens do jogador " + nomeCompleto + ":");
+        for(int i=0;i< itens.size();i++){
+            Item item = itens.get(i);
+            System.out.println(item);
+        }
+
+    }
 
 }
