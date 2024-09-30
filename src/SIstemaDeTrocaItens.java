e56c6dbe9c75a49c81a807c6e4bcce4500504f79
Guilherme Oliszewski <guilhermeoliszewski@outlook.com>
2024-09-30 09:53:34 -0300

    Criação da classe para o Sistema de Trocas

---
+++ src/SIstemaDeTrocaItens.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SIstemaDeTrocaItens {
+
+    private Jogador jogador = null;
+    private ArrayList<Jogador> jogadores = new ArrayList<>();
+
+    public void executar(){
+
+    }
+
+}
